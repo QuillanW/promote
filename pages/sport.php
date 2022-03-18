278c562b28585486ba4db0c46d845192464d88f8
Cefas88136 <95617801+Cefas88136@users.noreply.github.com>
2022-03-18 17:45:41 +0100

    Setup + start

---
+++ pages/sport.php
@@ -0,0 +1,49 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sport<!-- - sport --></title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="">archery</a></li>
+            <li><a href="">3x3 Baskedball</a></li>
+        </ul>
+    </nav>
+    
+    <header>
+        <picture>
+            <img width="200px" src="../media/sport/icons/archery.svg" alt="">
+        </picture>
+        <h1>Archery</h1>
+    </header>
+
+    <main>
+        <h2>Spotlights</h2>
+        <div class="spotlights"></div>
+
+        <div class="history">
+            <h2>
+                History of<br />
+                <!-- sport -->
+            </h2>
+            <!-- Content via php -->
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem dolorem, dolores ducimus earum maxime dolor, harum debitis quis, eos quia accusantium quibusdam quae corporis perferendis ratione. Quo vero repellat voluptate?
+                </p>
+                <h2>Archery in battle</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium, minima veniam numquam placeat assumenda quibusdam architecto ratione perferendis sapiente dolorem accusamus, rem autem quisquam quas voluptates. Molestiae reprehenderit illo dolore et dolores aperiam, accusantium dicta ullam eaque explicabo blanditiis omnis quo, eveniet similique consectetur tempora! Quia cupiditate minima, voluptatem, itaque commodi dolorum quasi odio, ipsam culpa maiores hic praesentium impedit ipsa repudiandae iusto! Dolore harum molestias error, accusamus in repellat omnis aspernatur explicabo veritatis placeat dolor est ea optio exercitationem?
+                </p>
+        </div>
+    </main>
+
+
+</body>
+</html>
